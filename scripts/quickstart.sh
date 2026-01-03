a96a494ff563f95eb734e6c6f7523870852e2a58
Joseph DiGiovanni <good.yard7176@jdigi.net>
2026-01-03 03:54:33 -0500

    Fix quickstart

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -22,20 +22,21 @@ sudo apt-get update
 # Setup wine
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install wine32
+sudo apt-get install -y wine32
 
 # Install MIPS assembler
-sudo apt-get install binutils-mips-linux-gnu
+sudo apt-get install -y binutils-mips-linux-gnu
+
+# Install Ninja build system
+sudo apt-get install -y ninja-build
 
 # Setup compiler
-sudo ./setup_progd_linux.sh
+./setup_prodg_linux.sh
 
 # Check if disc/SCUS_971.98 exists
 echo "Setup complete!"
 if [ ! -f ../disc/SCUS_971.98 ]; then
   echo "Now, copy SCUS_971.98 from your copy of the game to the 'disc' directory of this project."
-  echo "Then build the project by running the 'build.sh' script."
-  exit
 fi
 echo "To build the project, run the 'build.sh' script."
 
