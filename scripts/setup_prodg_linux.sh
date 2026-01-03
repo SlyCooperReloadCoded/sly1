22f5353949ed564be0430f14ef5177a3062cb09d
Joseph DiGiovanni <good.yard7176@jdigi.net>
2026-01-03 10:54:27 -0500

    Clean up ProDG setup

--- scripts/setup_prodg_linux.sh
+++ scripts/setup_prodg_linux.sh
@@ -1,38 +1,19 @@
 #!/bin/bash
-
 # Sets up the ProDG compilers + SCE Runtime Library to build the project.
-
 set -e
 
-WINE_ROOT=~/.wine/drive_c
 TOP=$(cd "$(dirname "$0")"; pwd)/..
-
-# The SDK (Runtime Library) version to install.
-SDK_VER=242
-
-die() { # perl-style `die` expressions.
-	echo "Error: $@"
-	exit 1
-}
 
 # downloads files without checking integrity
 download() {
 	echo "Downloading $1..."
 	wget -qP /tmp $1
-
-	BASENAME=$(basename $1)
-
-	#echo "moving files out of /tmp"
-	#mv /tmp/$BASENAME $TOP
 }
 
 echo Starting ProDG setup script...
 
 # download required files (registry + SDK package)
 download "https://github.com/TheOnlyZac/compilers/releases/download/ee-gcc2.95.2-SN-v2.73a/ee-gcc2.95.2-SN-v2.73a.tar.gz"
-
-# apply environment variables from the registry file
-# wine regedit prodg_env.reg
 
 # Extract the compiler into the tools dir
 echo "Extracting compiler to $TOP/tools..."
@@ -41,4 +22,4 @@ tar -xzf /tmp/ee-gcc2.95.2-SN-v2.73a.tar.gz -C $TOP/tools
 echo "Removing temporary files..."
 rm /tmp/ee-gcc2.95.2-SN-v2.73a.tar.gz
 
-echo "Setup complete!"
+echo "ProDG setup complete!"
